b79a2112e8e2a118c1b673406194a7caa2764186
Ketanest <87452756+Ketanest@users.noreply.github.com>
2026-04-09 00:41:55 +0200

    Update functions.php

--- functions.php
+++ functions.php
@@ -107,6 +107,7 @@ function calculate_seats($votes, $seats, $treshold = 0, $majority = 0){
 			//increase seats if majority is set and seats not majority
 			if($majorityparty != false){
 				while(true){
+					echo "while2";
 					if($seatcalc[$majorityparty]['seats'] < ceil(($seats / 2) + 0.1)){
 						$seats += 1;
 						$seatcalc[$majorityparty]['seats'] += 1;
