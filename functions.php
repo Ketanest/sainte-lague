8d8647e9ca70bbf4088390e8534d8d4502b10e68
Ketanest <87452756+Ketanest@users.noreply.github.com>
2026-04-08 13:15:21 +0200

    Create functions.php

---
+++ functions.php
@@ -0,0 +1,106 @@
+<?php
+/*
+fetches party and vote of any number (providing basic input check)
+returns array with partyname as key and votecount as value or NULL if array is empty
+*/
+function input_votes(){
+	$input = array();
+	
+	while(true){
+		$party = readline("Partei: (Enter fuer Beenden) ");
+		if($party == "") break;
+
+		repeat_votes:
+		$votes = readline("Stimmenanzahl: ");
+		if($votes == ""){
+			echo "Keine Eingabe, wiederholen!" . PHP_EOL;
+			goto repeat_votes;
+		} else if(!is_numeric($votes)){
+			echo "Eingabe ist keine Zahl, wiederholen!" . PHP_EOL;
+			goto repeat_votes;
+		} else {
+			$input[$party] = (int) $votes;
+		}
+	}
+
+	if(count($input) > 0) return $input; else return NULL;
+}
+
+/*
+fetches miscellaneous options (treshold, seats, majority)
+treshold (percent): integer between 0 and 100 (0 = off, 1-100 = percentage)
+seats (mandatory): integer seats in the parliament
+majority: bool if majority rule is applied
+*/
+function input_misc(){
+	repeat_treshold:
+	$input['treshold'] = (int) readline("Berücksichtigung einer Prozenthürde? (1-100, 0 für ohne [standard]) ");
+	if($input['treshold'] < 0 || $input['treshold'] > 100){
+		echo "Wert muss zwischen 0 und 100 liegen!" . PHP_EOL;
+		goto repeat_treshold;
+	}
+
+	$input['majority'] = (bool) readline("Berücksichtigung der Mehrheitsklausel? (ja / NEIN) ");
+
+	repeat_seats:
+	$input['seats'] = (int) readline("Wie viele Sitze sind zu vergeben (Ganze Zahl) ");
+	if(!($input['seats'] > 0)){
+		echo "Ungültige Eingabe, wiederholen!" . PHP_EOL;
+		goto repeat_seats;
+	}
+
+	return $input;
+}
+
+/*
+calculates seats proportionally
+treshold (optional, default: false): removes party not meeting the threshold
+majority (optional, default: false): more than 50 % of votes results mandatory in more than 50 % of the seats (increases seats if necessary)
+*/
+function calculate_seats($votes, $seats, $treshold = 0, $majority = 0){
+	$treshold_parties = array();
+	if($treshold > 0){
+		$sum = array_sum($votes);
+		foreach($votes as $party => $votecount){
+			$proportion = $votecount / $sum;
+			if($proportion < $treshold / 100){
+				$treshold_parties[$party] = $proportion;
+				 unset($votes[$party]);
+			}
+		}
+	}
+
+	$votesum = array_sum($votes);
+	echo "Start-Divisor: " . $divisor = $votesum / $seats;
+	$majorityparty = false;
+	$seatcalc = array();
+	$calcround = 1;
+	while(true){
+		foreach($votes as $party => $votecount){
+			$seatcalc[$party]['seats'] = (int) round($votecount / $divisor);
+			if($calcround == 1){
+				$seatcalc[$party]['proportion'] = $votecount / $votesum;
+				if($seatcalc[$party]['proportion'] > 0.5 && $majority) $majorityparty = $party;
+			}
+		}
+		$assigned_seats = array_sum(array_column($seatcalc, 'seats'));
+		if($assigned_seats == $seats){
+			if($majorityparty != false && $seatcalc[$majorityparty]['seats'] < ($seats / 2) + 1){
+				$seats += 1;
+			}else break;
+		}else if($assigned_seats > $seats){
+			echo "Divisor: " . $divisor += $divisor / 1000;
+		}else if($assigned_seats < $seats){
+			echo "Divisor: " . $divisor -= $divisor / 1000;
+		}else echo "irgenwo ist murks";
+		$calcround += 1;
+	}
+
+	foreach($treshold_parties as $party => $proportion){
+		$seatcalc[$party]['seats'] = 0;
+		$seatcalc[$party]['proportion'] = $proportion;
+	}
+
+	return $seatcalc;
+}
+?>
