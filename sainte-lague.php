4d95a278d4d60ffa9333e101b0c9219f202d5ca9
Ketanest <87452756+Ketanest@users.noreply.github.com>
2026-04-08 23:44:33 +0200

    Update sainte-lague.php

--- sainte-lague.php
+++ sainte-lague.php
@@ -5,7 +5,7 @@ include("functions.php");
 #$votes = input_votes();
 #$misc = input_misc();
 
-$votes = array("GRUEN" => 991478, "CSU" => 622986, "FDP" => 133257, "SPD" => 189327, "Linke" => 17634);
+$votes = array("GRUEN" => 990478, "CSU" => 622986, "FDP" => 133257, "SPD" => 189327, "Linke" => 17634);
 $result = calculate_seats($votes, 70, 5, 1);
 
 #$result = calculate_seats($votes, $misc['seats'], $misc['treshold'], $misc['majority']);
